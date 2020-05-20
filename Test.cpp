6e59b98553e62d32036c7b687ed184dac45a6fcf
edutco <edut97@gmail.com>
2020-05-20 15:48:39 +0300

    test file

---
+++ Test.cpp
@@ -0,0 +1,13 @@
+#include "doctest.h"
+#include "Soldier.hpp"
+#include "Board.hpp"
+using namespace std;
+#include <string>
+
+
+// Id of collaborators 208825539 207950577
+
+TEST_CASE("Test replacement of p, f and b") {
+
+   // CHECK(find(text, "haffy") == string("happy"));
+}
